38699a7d88219685b07d9e30cc43966b140c54fb
vivek rakhunde <dev.vivek@mejidigital.in>
2023-12-16 19:05:30 +0530

    game over updated

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,15 +1,12 @@
 export default function Log({turns}){
-    console.log(turns,'turns')
+   
     return (
         <ol id="log">
             {
                 turns.map((turn)=>(
-                    <li key={`${turn.square.row} ${turn.square.col}`}>
-                    {turn.player} selected {turn.square.row},{turn.square.col}
-                    </li>
+                    <li key={`${turn.square.tow}${turn.square.col}`} >{turn.player} selected {turn.square.row},{turn.square.col}</li>
                 ))
             }
-
         </ol>
-    )
+    ) 
 }
